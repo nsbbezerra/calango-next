67d39066ad581f304e6a6f06ce0198c241822e16
Natanael Bezerra <nsbbezerra@hotmail.com>
2021-03-18 15:54:56 -0300

    insert forms

--- components/header.js
+++ components/header.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Head from "next/head";
 import {
   Box,
@@ -12,12 +13,39 @@ import {
   MenuButton,
   MenuList,
   MenuItem,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Text,
+  Icon,
+  FormControl,
+  FormLabel,
+  Input,
+  Grid,
+  InputLeftElement,
+  InputGroup,
+  Select,
 } from "@chakra-ui/react";
-import { FaWhatsapp, FaInstagram, FaBars } from "react-icons/fa";
+import {
+  FaWhatsapp,
+  FaInstagram,
+  FaBars,
+  FaUserAlt,
+  FaSave,
+} from "react-icons/fa";
 import Image from "next/image";
 import Link from "next/link";
+import MaskedInput from "react-text-mask";
+import { AiOutlineLogin } from "react-icons/ai";
 
 export default function HeaderApp() {
+  const [modalLogin, setModalLogin] = useState(false);
+  const [modalRegister, setModalRegister] = useState(false);
+
   return (
     <>
       <Head>
@@ -66,11 +94,41 @@ export default function HeaderApp() {
               <Menu placement="bottom-end">
                 <MenuButton
                   as={IconButton}
-                  icon={<FaBars />}
+                  icon={<FaUserAlt />}
                   size="lg"
                   colorScheme="purple"
                   variant="outline"
                   fontSize="2xl"
+                >
+                  Área do Cliente
+                </MenuButton>
+                <MenuList shadow="lg" borderWidth="2px" borderColor="green.400">
+                  <MenuItem
+                    _active={{ bg: "purple.100", color: "white" }}
+                    _focus={{ bg: "transparent" }}
+                    _hover={{ bg: "purple.100", color: "white" }}
+                    onClick={() => setModalRegister(true)}
+                  >
+                    CADASTRE-SE
+                  </MenuItem>
+                  <MenuItem
+                    _active={{ bg: "purple.100", color: "white" }}
+                    _focus={{ bg: "transparent" }}
+                    _hover={{ bg: "purple.100", color: "white" }}
+                    onClick={() => setModalLogin(true)}
+                  >
+                    FAÇA LOGIN
+                  </MenuItem>
+                </MenuList>
+              </Menu>
+              <Menu placement="bottom-end">
+                <MenuButton
+                  as={IconButton}
+                  icon={<FaBars />}
+                  size="lg"
+                  colorScheme="purple"
+                  fontSize="2xl"
+                  ml={3}
                 />
                 <MenuList shadow="lg" borderWidth="2px" borderColor="green.400">
                   <MenuItem
@@ -105,67 +163,375 @@ export default function HeaderApp() {
               </Menu>
             </Flex>
             <Flex
+              justify="space-between"
+              direction="column"
               align="flex-end"
               h="100px"
               display={["none", "none", "flex", "flex", "flex"]}
             >
-              <Button
-                colorScheme="purple"
-                variant="link"
-                size="sm"
-                _focus={{ outline: "none" }}
-              >
-                SORTEIOS
-              </Button>
-              <Divider
-                orientation="vertical"
-                h="20px"
-                ml={3}
-                mr={3}
-                borderColor="purple.400"
-              />
-              <Button
-                colorScheme="purple"
-                variant="link"
-                size="sm"
-                _focus={{ outline: "none" }}
-              >
-                FALE CONOSCO
-              </Button>
-              <Divider
-                orientation="vertical"
-                h="20px"
-                ml={3}
-                mr={3}
-                borderColor="purple.400"
-              />
-              <Button
-                colorScheme="purple"
-                variant="link"
-                size="sm"
-                _focus={{ outline: "none" }}
-              >
-                CRIAR SORTEIO
-              </Button>
-              <Divider
-                orientation="vertical"
-                h="20px"
-                ml={3}
-                mr={3}
-                borderColor="purple.400"
-              />
-              <Button
-                colorScheme="purple"
-                variant="link"
-                size="sm"
-                _focus={{ outline: "none" }}
-              >
-                CONDIÇÔES DE USO
-              </Button>
+              <Menu placement="bottom-end">
+                <MenuButton
+                  as={Button}
+                  leftIcon={<FaUserAlt />}
+                  colorScheme="purple"
+                  variant="outline"
+                  maxW="max-content"
+                  mt={3}
+                >
+                  Área do Cliente
+                </MenuButton>
+                <MenuList shadow="lg" borderWidth="2px" borderColor="green.400">
+                  <MenuItem
+                    _active={{ bg: "purple.100", color: "white" }}
+                    _focus={{ bg: "transparent" }}
+                    _hover={{ bg: "purple.100", color: "white" }}
+                    onClick={() => setModalRegister(true)}
+                  >
+                    CADASTRE-SE
+                  </MenuItem>
+                  <MenuItem
+                    _active={{ bg: "purple.100", color: "white" }}
+                    _focus={{ bg: "transparent" }}
+                    _hover={{ bg: "purple.100", color: "white" }}
+                    onClick={() => setModalLogin(true)}
+                  >
+                    FAÇA LOGIN
+                  </MenuItem>
+                </MenuList>
+              </Menu>
+              <Flex>
+                <Button
+                  colorScheme="purple"
+                  variant="link"
+                  size="sm"
+                  _focus={{ outline: "none" }}
+                >
+                  SORTEIOS
+                </Button>
+                <Divider
+                  orientation="vertical"
+                  h="20px"
+                  ml={3}
+                  mr={3}
+                  borderColor="purple.400"
+                />
+                <Button
+                  colorScheme="purple"
+                  variant="link"
+                  size="sm"
+                  _focus={{ outline: "none" }}
+                >
+                  FALE CONOSCO
+                </Button>
+                <Divider
+                  orientation="vertical"
+                  h="20px"
+                  ml={3}
+                  mr={3}
+                  borderColor="purple.400"
+                />
+                <Button
+                  colorScheme="purple"
+                  variant="link"
+                  size="sm"
+                  _focus={{ outline: "none" }}
+                >
+                  CRIAR SORTEIO
+                </Button>
+                <Divider
+                  orientation="vertical"
+                  h="20px"
+                  ml={3}
+                  mr={3}
+                  borderColor="purple.400"
+                />
+                <Button
+                  colorScheme="purple"
+                  variant="link"
+                  size="sm"
+                  _focus={{ outline: "none" }}
+                >
+                  CONDIÇÔES DE USO
+                </Button>
+              </Flex>
             </Flex>
           </Flex>
         </Container>
       </Box>
+
+      <Modal
+        isOpen={modalLogin}
+        onClose={() => setModalLogin(false)}
+        isCentered
+        size="sm"
+      >
+        <ModalOverlay />
+        <ModalContent borderWidth="3px" borderColor="green.400">
+          <ModalHeader>
+            <Flex align="center">
+              <Icon as={AiOutlineLogin} />
+              <Text ml={3}>Login</Text>
+            </Flex>
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl>
+              <FormLabel>CPF</FormLabel>
+              <MaskedInput
+                mask={[
+                  /[0-9]/,
+                  /\d/,
+                  /\d/,
+                  ".",
+                  /\d/,
+                  /\d/,
+                  /\d/,
+                  ".",
+                  /\d/,
+                  /\d/,
+                  /\d/,
+                  "-",
+                  /\d/,
+                  /\d/,
+                ]}
+                placeholder="CPF"
+                render={(ref, props) => (
+                  <Input ref={ref} {...props} focusBorderColor="purple.400" />
+                )}
+              />
+            </FormControl>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="purple" leftIcon={<AiOutlineLogin />}>
+              Login
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+
+      <Modal
+        isOpen={modalRegister}
+        onClose={() => setModalRegister(false)}
+        size="3xl"
+      >
+        <ModalOverlay />
+        <ModalContent borderWidth="3px" borderColor="green.400">
+          <ModalHeader>
+            <Flex align="center">
+              <Icon as={FaSave} />
+              <Text ml={3}>Cadastro</Text>
+            </Flex>
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Grid templateColumns="1fr" gap="15px">
+              <FormControl>
+                <FormLabel>Nome Completo</FormLabel>
+                <Input focusBorderColor="purple.400" />
+              </FormControl>
+            </Grid>
+            <Grid
+              mt={3}
+              templateColumns={[
+                "1fr",
+                "repeat(2, 1fr)",
+                "repeat(2, 1fr)",
+                "repeat(2, 1fr)",
+                "repeat(2, 1fr)",
+              ]}
+              gap="15px"
+            >
+              <FormControl>
+                <FormLabel>CPF</FormLabel>
+                <MaskedInput
+                  mask={[
+                    /[0-9]/,
+                    /\d/,
+                    /\d/,
+                    ".",
+                    /\d/,
+                    /\d/,
+                    /\d/,
+                    ".",
+                    /\d/,
+                    /\d/,
+                    /\d/,
+                    "-",
+                    /\d/,
+                    /\d/,
+                  ]}
+                  placeholder="CPF"
+                  render={(ref, props) => (
+                    <Input ref={ref} {...props} focusBorderColor="purple.400" />
+                  )}
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Telefone</FormLabel>
+                <MaskedInput
+                  mask={[
+                    "(",
+                    /[0-9]/,
+                    /\d/,
+                    ")",
+                    " ",
+                    /\d/,
+                    /\d/,
+                    /\d/,
+                    /\d/,
+                    /\d/,
+                    "-",
+                    /\d/,
+                    /\d/,
+                    /\d/,
+                    /\d/,
+                  ]}
+                  placeholder="Telefone"
+                  id="contact"
+                  render={(ref, props) => (
+                    <InputGroup>
+                      <InputLeftElement children={<FaWhatsapp />} />
+                      <Input
+                        placeholder="Telefone"
+                        ref={ref}
+                        {...props}
+                        focusBorderColor="purple.400"
+                      />
+                    </InputGroup>
+                  )}
+                />
+              </FormControl>
+            </Grid>
+            <Divider mt={7} mb={4} />
+            <Grid
+              templateColumns={[
+                "1fr",
+                "3fr 1fr",
+                "3fr 1fr",
+                "3fr 1fr",
+                "3fr 1fr",
+              ]}
+              gap="15px"
+            >
+              <FormControl>
+                <FormLabel>
+                  Logradouro - Rua, Avenida, Alameda, etc...
+                </FormLabel>
+                <Input focusBorderColor="purple.400" />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Número</FormLabel>
+                <Input focusBorderColor="purple.400" />
+              </FormControl>
+            </Grid>
+            <Grid
+              templateColumns={[
+                "1fr",
+                "1fr 1fr",
+                "1fr 1fr",
+                "1fr 1fr",
+                "1fr 1fr",
+              ]}
+              mt={3}
+              gap="15px"
+            >
+              <FormControl>
+                <FormLabel>Ponto de Referência</FormLabel>
+                <Input focusBorderColor="purple.400" />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Bairro / Distrito</FormLabel>
+                <Input focusBorderColor="purple.400" />
+              </FormControl>
+            </Grid>
+            <Grid
+              templateColumns={[
+                "1fr",
+                "1fr 2fr 1fr",
+                "1fr 2fr 1fr",
+                "1fr 2fr 1fr",
+                "1fr 2fr 1fr",
+              ]}
+              mt={3}
+              gap="15px"
+            >
+              <FormControl>
+                <FormLabel>CEP</FormLabel>
+                <MaskedInput
+                  mask={[
+                    /[0-9]/,
+                    /\d/,
+                    ".",
+                    /\d/,
+                    /\d/,
+                    /\d/,
+                    "-",
+                    /\d/,
+                    /\d/,
+                    /\d/,
+                  ]}
+                  placeholder="CEP"
+                  render={(ref, props) => (
+                    <Input
+                      ref={ref}
+                      {...props}
+                      focusBorderColor={"purple.400"}
+                    />
+                  )}
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel>Cidade</FormLabel>
+                <Input focusBorderColor="purple.400" />
+              </FormControl>
+              <FormControl>
+                <FormLabel>UF</FormLabel>
+                <Select
+                  placeholder="Selecione"
+                  variant="outline"
+                  focusBorderColor={"purple.400"}
+                  id="state"
+                >
+                  <option value="AC">AC</option>
+                  <option value="AL">AL</option>
+                  <option value="AP">AP</option>
+                  <option value="AM">AM</option>
+                  <option value="BA">BA</option>
+                  <option value="CE">CE</option>
+                  <option value="DF">DF</option>
+                  <option value="ES">ES</option>
+                  <option value="GO">GO</option>
+                  <option value="MA">MA</option>
+                  <option value="MT">MT</option>
+                  <option value="MS">MS</option>
+                  <option value="MG">MG</option>
+                  <option value="PA">PA</option>
+                  <option value="PB">PB</option>
+                  <option value="PR">PR</option>
+                  <option value="PE">PE</option>
+                  <option value="PI">PI</option>
+                  <option value="RJ">RJ</option>
+                  <option value="RN">RN</option>
+                  <option value="RS">RS</option>
+                  <option value="RO">RO</option>
+                  <option value="RR">RR</option>
+                  <option value="SC">SC</option>
+                  <option value="SP">SP</option>
+                  <option value="SE">SE</option>
+                  <option value="TO">TO</option>
+                </Select>
+              </FormControl>
+            </Grid>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="purple" leftIcon={<FaSave />}>
+              Cadastrar
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   );
 }
